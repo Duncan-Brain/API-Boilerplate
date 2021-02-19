ce5146e2fca1f5381167e9f7f018eed57eaaeb43
Mr-BrainSpace <duncan@brainenterprise.ca>
2021-02-19 17:05:03 -0500

    IfNotPresent?

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-docker build -t tempfrontend -t duncanbrain/boiler-api-frontend:latest -t duncanbrain/boiler-api-frontend:$SHA -f ./frontend/Dockerfile.next ./frontend
-docker build -t tempapi -t duncanbrain/boiler-api-api:latest -t duncanbrain/boiler-api-api:$SHA -f ./api/Dockerfile ./api
+docker build -t duncanbrain/boiler-api-frontend:latest -t duncanbrain/boiler-api-frontend:$SHA -f ./frontend/Dockerfile.next ./frontend
+docker build -t duncanbrain/boiler-api-api:latest -t duncanbrain/boiler-api-api:$SHA -f ./api/Dockerfile ./api
 
 docker push duncanbrain/boiler-api-frontend:latest
 docker push duncanbrain/boiler-api-api:latest
@@ -8,5 +8,5 @@ docker push duncanbrain/boiler-api-frontend:$SHA
 docker push duncanbrain/boiler-api-api:$SHA
 
 kubectl --kubeconfig=$HOME/.kube/config apply -f k8s
-kubectl --kubeconfig=$HOME/.kube/config set image deployment/frontend-deployment frontend-deployment=tempfrontend
-kubectl --kubeconfig=$HOME/.kube/config set image deployment/api-deployment api-deployment=tempapi
+kubectl --kubeconfig=$HOME/.kube/config set image deployment/frontend-deployment frontend=duncanbrain/boiler-api-frontend:$SHA
+kubectl --kubeconfig=$HOME/.kube/config set image deployment/api-deployment api=duncanbrain/boiler-api-api:$SHA
