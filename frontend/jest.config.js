11b2d359dc3949eed5171c8a59fda884ce02f0f6
Mr-BrainSpace <duncan@brainenterprise.ca>
2021-02-24 16:49:58 -0500

    altered to watchAll

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -13,6 +13,11 @@ module.exports = {
     "**/__tests__/**/*.[jt]s?(x)",
     "**/?(*.)+(spec|test).[jt]s?(x)"
   ],
+  testPathIgnorePatterns: [
+    '/.next/',
+    '/node_modules/',
+    '/testconfig/'
+  ],
   transform: {
     '\\.js$': ['babel-jest', { configFile: './testconfig/.babelrc.json' }]
   },
